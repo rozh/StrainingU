41f3f2d6c8997c4a247ccb665cc053a392f9046c
奥西 亮賀 <okunishiakiraga@ryoga-iMac.local>
2013-09-23 08:12:07 +0900

    jsとchromeapiの使い方わからぬ

--- myscript.js
+++ myscript.js
@@ -1,1 +1,16 @@
-alert("You must not see this WebSite");
+//alert("You must not see this WebSite");
+
+chrome.tabs.getSelected(null, function(tab) {  
+  if (tab.url.indexOf('http://www.amazon.co.jp/*') == 0 ||  
+    tab.url.indexOf('https://www.amazon.co.jp/*') == 0) {  
+    // Amazon のページのときの処理
+    alert("hoge");
+    document.getElementById('msg').innerHTML = tab.title;  
+  }  
+  else {  
+    // Amazon 以外のページ  
+    alert("foo");
+    document.getElementById('msg').innerHTML = "";  
+  }  
+}  
+
